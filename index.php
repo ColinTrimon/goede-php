99852d02a412673bc7d788d8217f4bf79a2e9fad
Colin de Wit <99034809@mydavinci.nl>
2018-01-24 14:53:58 +0100

    Animatie toegevoegt

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+date_default_timezone_set("Europe/Amsterdam");
+date_default_timezone_get();
+$time = date_default_timezone_get;
+
+echo "<p id='currently'>Currently it is:</p>". date("H:i:sa d-m-Y<br>");
+
+$date = date("H");
+
+if ($date < 6) {
+  $tekst= "<p>Goodnight Sweetheart!</p>";
+  $body= 'night.png';
+  $animationimg=  "<image id='ufo' src='backgrounds/ufo.png' alt='ufo'>";
+  
+}
+elseif($date < 12) {
+  $tekst= "<p>Goodmorning Sweetheart!</p>";
+  $body= 'morning.png';
+}
+elseif ($date < 18) {
+  $tekst= "<p>Goodafternoon Sweetheart!</p>";
+  $body= 'afternoon.png';
+  $animationimg= "<image id='plane' src='backgrounds/plane.png' alt='plane'>";
+}
+else{
+  $tekst= "<p>Goodevening Sweetheart!</p>";
+  $body= 'evening.png';
+}
+?>
 <!DOCTYPE html>
 <html lang="zxx">
   <head>
@@ -6,35 +35,11 @@
     <title>Goede</title>
     <link rel="stylesheet" href="style.css" type="text/css">
   </head>
-  <body>
+  <body background="backgrounds/<?php echo $body; ?>">
     <?php
-    date_default_timezone_set("Europe/Amsterdam");
-    date_default_timezone_get();
-    $time = date_default_timezone_get;
-    $page = $_SERVER['PHP_SELF'];
-      $sec = "1";
-      header("Refresh: $sec; url=$page");
 
-    echo "<p id='currently'>Currently it is:</p>". date("H:i:sa <br>");
-
-    $date = date("H");
-
-    if ($date < 5) {
-      echo "<p>GoodnightSweetheart!</p>";
-      echo '<body background="backgrounds/night.png">';
-    }
-    elseif($date < 12) {
-      echo "<p>Goodmorning Sweetheart!</p>";
-      echo '<body background="backgrounds/morning.png">';
-    }
-    elseif ($date < 18) {
-      echo "<p>Goodafternoon Sweetheart!</p>";
-      echo '<body background="backgrounds/afternoon.png">';
-    }
-    else{
-      echo "<p>Goodevening Sweetheart!</p>";
-      echo '<body background="backgrounds/evening.png">';
-    }
+    echo $tekst;
+    echo $animationimg;
     ?>
   </body>
 </html>
